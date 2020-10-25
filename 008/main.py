2dd6d29e666b0e965be4a4f1a86c3adb5363e4a1
krisztikelemen <71121217+krisztikelemen@users.noreply.github.com>
2020-10-25 21:07:13 +0100

    Feladat megoldva.

--- 008/main.py
+++ 008/main.py
@@ -1,13 +1,21 @@
 x=0.0
 y=0.0
 
+irany = input()
 
+while irany != "stop":
+  if irany == "forward":
+    y = y + float(input())
+  if irany == "backward":
+    y = y - float(input())
+  if irany == "right":
+    x = x + float(input())
+  if irany == "left":
+    x = x - float(input())
+  irany = input()
 
-
-
-
-
-
-print(round(x,2))
-print(round(y,2))
-    
+if irany == "stop":
+  print(round(x,2))
+  print(round(y,2))
+  origo = ((x**2)+(y**2))**(1/2)
+  print(round(origo,2))
