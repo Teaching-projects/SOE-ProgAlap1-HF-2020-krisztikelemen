9a5c6f0142fe10586db2e7d35c13c9f1fa8e8930
krisztikelemen <71121217+krisztikelemen@users.noreply.github.com>
2020-10-29 15:46:10 +0100

    Feladat megoldva.

--- 010/main.py
+++ 010/main.py
@@ -1,13 +1,21 @@
+perc=[]
+sms=[]
+list=[]
 
+for i in range(12):
+  perc.append(int(input()))
+  sms.append(int(input()))
 
+havidij=int(input())
+percdij=int(input())
+smsdij=int(input())
 
+for honap in range(12):
+  szamla = (percdij*perc[honap]) + (smsdij*sms[honap])
+  if szamla > havidij:
+    list.append(szamla)
+  else: 
+    list.append(havidij)
 
-szamla=[]
-
-
-
-
-print(szamla)
-print(total)
-
-
+print(list)
+print(sum(list))
